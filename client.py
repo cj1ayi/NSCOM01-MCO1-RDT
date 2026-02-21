cbac42f87bf7cc1e5e0d95081889c53f80ee0735
kekekoby <jacobaluna8@gmail.com>
2026-02-21 18:02:31 +0800

    SYN and FIN functionality finished. Neef to edit server to not be class

--- client.py
+++ client.py
@@ -1,80 +1,98 @@
 """ Main client file """
 import socket
 
-class Client:
-	def __init__(self):
-		# Creating client socket, creating var for server address
-		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		self.server_addr = ('127.0.0.1', 8888)
+# INITIALIZATION =================================================
+# Creating client socket, creating var for server address
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_addr = ('127.0.0.1', 8888)
 
-		# Port set to 0 so OS gives any avail port
-		self.sock.bind(('', 0))
-
-		# Moving to main loop
-		self.handshake() # Or handshake loop?
-
-	def __del__(self):
-		# Close socker
-		self.sock.close()
-
-	def handshake(self):
-		# SYN and SYNACK trading. Check if server is active, basically
-		print("Attempting to reach server...")
-		self.sock.settimeout(3)
-
-		# Three attempts to reach server until client gives up.
-		connected = False
-		for i in range(3):
-			try:
-				SYN = (b"\x00")
-				self.sock.sendto(SYN, self.server_addr)
-
-				data, _ = self.sock.recvfrom(1024)
-				if int.from_bytes(data[:2]) == 1:
-					connected = True
-					break
-
-			except socket.timeout:
-				print("Failed to reach server. Retrying...")
-
-		if not connected:
-			print("Failed to reach server. Please check if server is running.")
-		else:
-			print("\nServer connected!")
-			self.main() # Go to main loop
-
-	def main(self):
-		# main loop
-		while True:
-			print("\n\n================================================")
-			print("\nSelect action:")
-			print("[1] Download File")
-			print("[2] Upload File")
-			print("[3] Exit")
-			print()
-
-			action_chosen = input(">>> ").strip()
-
-			match action_chosen:
-				case "1":
-					#download
-					print("downlao")
-
-				case "2":
-					print("upload")
-					#upload
-
-				case "3":
-					#exit
-					print("Exiting program...")
-					return
-
-				case _:
-					#incorrect input
-					print("Something seems to be wrong with your input.\n",
-						  "Press ENTER to continue.")
-					input()
+# Port set to 0 so OS gives any avail port
+sock.bind(('', 0))
 
 
-if __name__ == "__main__":
-	client = Client()
+
+# HANDSHAKE (SYN/SYNACK) ========================================
+# SYN and SYNACK trading. Check if server is active, basically
+print("Attempting to reach server...")
+sock.settimeout(3)
+
+# Three attempts to reach server until client gives up.
+connected = False
+for i in range(3):
+	try:
+		SYN = (b"\x00")
+		sock.sendto(SYN, server_addr)
+
+		data, _ = sock.recvfrom(1024)
+		if int.from_bytes(data[:2]) == 1:
+			connected = True
+			break
+
+	except socket.timeout:
+		print("Failed to reach server. Retrying...")
+
+if not connected:
+	print("Failed to reach server. Please check if server is running.")
+else:
+	print("\nServer connected!")
+
+
+
+# TERMINATION ====================================================
+# Closes socket and sends FIN
+def close_socket():
+
+	# Sending FIN. Three attempts before giving up
+	fin_sent = False
+	sock.settimeout(5)
+	for i in range(3):
+		try:
+			FIN = (b'\x07')
+			sock.sendto(FIN, server_addr)
+
+			data, _ = sock.recvfrom(1024)
+			if int.from_bytes(data[:2]) == 8:
+				fin_sent = True
+				break
+
+		except socket.timeout:
+			print("Failed to reach server. Retrying...")
+
+	if not fin_sent:
+		print("Error in reaching server. Server may be closed.")
+
+	sock.close()
+
+
+
+# MAIN LOOP =======================================================
+while True and connected:
+	print("\n\n================================================")
+	print("\nSelect action:")
+	print("[1] Download File")
+	print("[2] Upload File")
+	print("[3] Exit")
+	print()
+
+	action_chosen = input(">>> ").strip()
+
+	match action_chosen:
+		case "1":
+			#download
+			print("downlao")
+
+		case "2":
+			print("upload")
+			#upload
+
+		case "3":
+			print("Exiting program...")
+			close_socket()
+			connected = False
+
+		case _:
+			#incorrect input
+			print("Something seems to be wrong with your input.\n",
+				  "Press ENTER to continue.")
+			input()
+
--- server.py
+++ server.py
@@ -12,6 +12,9 @@ class Server:
 			TFTP uses port 69 but maybe since this is supposed to be our own protocol
 			it can be different? But anws just consider it a placeholder for now """
 
+		# Session info is stored here
+		self.sessions = {}
+
 		self.loop() # Move to main server loop
 
 	def __del__(self):
@@ -25,11 +28,39 @@ class Server:
 			data, addr = self.sock.recvfrom(1024)
 			print("Received packet from ", addr)
 
-			if int.from_bytes(data[:2]) == 0:
+
+			if int.from_bytes(data[:2]) == 0: # SYN
 				print("\nReceived SYN from ", addr)
 
-				SYNACK = (b'\x01')
-				self.sock.sendto(SYNACK, addr)
+				# Adding new client to sessions dictionary
+				if addr in self.sessions:
+					print("Error: Unexpected packet") # ------------------- error
+					# TODO: Send error packet
+				else:
+					self.sessions[addr] = {}
+					print(self.sessions)
+					print(addr, " added to sessions!")
+
+					SYNACK = (b'\x01')
+					self.sock.sendto(SYNACK, addr)
+
+
+			elif int.from_bytes(data[:2]) == 7: # FIN
+				print("\nReceived FIN from ", addr)
+
+				# Removing client from sessions dictionary
+				if addr not in self.sessions:
+					print("Error: Unexpected packet") # -------------------- error
+					# TODO: Send error packet
+				else:
+					self.sessions.pop(addr)
+					print(self.sessions)
+					print(addr, " removed from sessions.")
+
+					FINACK = (b'\x08')
+					self.sock.sendto(FINACK, addr)
+
+
 
 if __name__ == "__main__":
 	server = Server()
